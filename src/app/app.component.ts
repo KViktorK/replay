4908565f23c1a21dac621cbba9fbd70c7fddb3a7
Viktor <viktorkalyn1999@gmail.com>
2022-07-13 04:49:34 +0200

    Create task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { bids, IBid } from './bid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+  constructor(private datePipe: DatePipe) {}
+  bidToShow: IBid = bids[0];
+  CURRENT_INDEX: number = 0;
+  LAST_INDEX: number = bids.length - 1;
+  secondsCounter = interval(Math.random()*4000);
+
+  convertDate(mil: number) {
+    const convertedDate = this.datePipe.transform(mil, 'MMM d, y, h:mm:ss a');
+    return convertedDate;
+  }
+  ngOnInit() {
+    const subscription = this.secondsCounter.subscribe((n) => {
+      this.CURRENT_INDEX += 1;
+      this.bidToShow = bids[this.CURRENT_INDEX];
+      if (this.CURRENT_INDEX === this.LAST_INDEX) {
+        console.log('UNSUBSCRIBED EVENT');
+        subscription.unsubscribe();
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
+import { DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [DatePipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
